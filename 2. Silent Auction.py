4378d171371ca92f710dcf91f79f66808589823c
sychoe06 <83557960+sychoe06@users.noreply.github.com>
2022-02-28 14:58:32 +1300

    2. Silent Auction

---
+++ 2. Silent Auction.py
@@ -0,0 +1,4 @@
+item_being_auctioned = input("What is the auction for? ")
+reserve_price = input("What is the reserve price? ")
+print(f"The auction for the {item_being_auctioned} has started!")
+print("")
